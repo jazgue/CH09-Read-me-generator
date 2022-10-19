30ddbe2ba8030e420e7e0a6e7d84659d0ae6e35f
Jazzy <jazmiwg@gmail.com>
2022-10-19 13:53:45 -0700

    Added code to index.js

---
+++ README.md
@@ -0,0 +1,26 @@
+# undefined
+
+  ## Licensing:
+  [![license](https://img.shields.io/badge/license--blue)](https://shields.io)
+  ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Additional Info](#additional-info)
+  ## Description:
+  This project is created to make it easier for the user to create a read me file .
+  ## Installation:
+  Clone the repository
+  ## Usage:
+  Run node index.js
+  ## License:
+  
+  ## Contribution:
+  NA
+  ## Testing:
+  Run node index.js then answer the questions
+  ## Additional Info:
+  - Github: [jazgue](https://github.com/jazgue)
+  - Email: jazmiwg@gmail.com 
--- index.js
+++ index.js
@@ -1,13 +1,149 @@
 // TODO: Include packages needed for this application
+const inquirer = require ('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the project?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('You need to enter a name to continue');
+                return false;
+            }
+        }
+    },
+  
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('You need to enter a project description');
+                return false;
+            }
+        }
+    },
+ 
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Instrtuctions on how to install the project?',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('You need to provide installation info to continue');
+                return false;
+            }
+        }
+    },
+    
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this project?',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('You need to provide information on how to use project');
+                return false;
+            }
+        }
+    },
+  
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How should people contribute to this project?',
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('You need to provide information on how to contribute to the project');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'How do you test this project?',
+        validate: testingInput => {
+            if (testingInput) {
+                return true;
+            } else {
+                console.log('You need to describe how to test this project');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'checkbox',
+        name: 'licensing',
+        message: 'Choose a license for your project',
+        choices: ['MIT', 'Apache', 'GNU-General-Public', 'Mozilla-Public', 'Common-Development-and Distribution', 'None'],
+        validate: licensingInput => {
+            if (licensingInput) {
+                return true;
+            } else {
+                console.log('You must pick a license for the project');
+                return false;
+            }
+        }
+    },
+   
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err)
+            throw err;
+        console.log('File written!')
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function (userInput) {
+        console.log(userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+    });
+}
 
 // Function call to initialize app
 init();
